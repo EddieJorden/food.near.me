039e1b02e0afa479b8c74bc495d70e6f62b63e28
Eddie Jorden <eddiejorden.tech@gmail.com>
2021-04-03 13:24:56 -0700

    created businessList.js

---
+++ src/components/Business/BusinessList/BusinessList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import BusinessList from './BusinessList.css';
+import Business from '../Business/Business.js';
+
+const BusinessList = () => {
+    render() {
+        return(
+            <div className="BusinessList">
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+            <Business />
+            </div>
+        )
+    }
+}
