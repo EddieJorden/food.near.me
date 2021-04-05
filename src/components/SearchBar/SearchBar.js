10bb8e4c9303a833b59f1aa86eedad65b865b337
Eddie Jorden <eddiejorden.tech@gmail.com>
2021-04-05 06:43:38 -0700

    search bar and sort by options state setup up and working properly --ej--

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -9,6 +9,10 @@ class SearchBar extends React.Component {
 			location: '',
 			sortBy: 'best_match',
 		};
+
+		this.handleTermChange = this.handleTermChange.bind(this);
+		this.handleLocationChange = this.handleLocationChange.bind(this);
+
 		this.sortByOptions = {
 			'Best Match': 'best_match',
 			'Highest Rated': 'rating',
@@ -39,7 +43,7 @@ class SearchBar extends React.Component {
 	}
 
 	handleSortByChange(sortByOption) {
-		this.setState(sortByOption);
+		this.setState({ sortBy: sortByOption });
 	}
 
 	handleTermChange(event) {
@@ -47,7 +51,7 @@ class SearchBar extends React.Component {
 	}
 
 	handleLocationChange(event) {
-		this.setState({ lcation: event.target.value });
+		this.setState({ location: event.target.value });
 	}
 
 	render() {
@@ -57,8 +61,8 @@ class SearchBar extends React.Component {
 					<ul>{this.renderSortByOptions()}</ul>
 				</div>
 				<div className="SearchBar-fields">
-					<input placeholder="Search Businesses" />
-					<input placeholder="Where?" />
+					<input onChange={handleTermChange} placeholder="Search Businesses" />
+					<input onChange={handleLocationChange} placeholder="Where?" />
 				</div>
 				<div className="SearchBar-submit">
 					<a href="www.#.com">Let's Go</a>
